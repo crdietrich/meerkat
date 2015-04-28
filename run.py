f1ed49378e782396a8f79518953a297078108d2b
alan <alan@fancysandwiches.com>
2015-04-28 01:54:38 -0700

    Implement a better run loop. Stub out other functions. Add lots of TODOs

--- run.py
+++ run.py
@@ -1,46 +1,90 @@
 import serial
 import threading
+from io import BufferedRWPair, TextIOWrapper
+from time import sleep
 
-print('Starting server...')
 
-temperature_usb = '/dev/ttyAMA0'
+temp_usb = '/dev/ttyAMA0'
 BAUD_RATE = 9600
-temperature_ser = serial.Serial(temperature_usb, BAUD_RATE)
 
-def process_line(line):
-	print('Temp: {}'.format(line))
 
-def temperature_loop():
-	line = ""
+def init_db():
+	# TODO: initialize the sqlite database.
+	pass
+
+
+def create_connection(usb_port):
+	ser = serial.Serial(usb_port, BAUD_RATE)
+	disable_continuous_mode(ser)
+	return TextIOWrapper(BufferedRWPair(ser, ser), newline='\r', encoding='ascii', line_buffering=True)
+
+
+def disable_continuous_mode(conn: serial.Serial):
+	# TODO: research if we need to send this command every time we connect to the sensors, or if it only
+	# needs to be sent once to disable continuous mode. If only once we should move this code into a
+	# separate python file.
+	
+	conn.write('E\r')
+
+	if conn.inWaiting() > 0:
+		# clear the buffer if there is anything waiting.
+		conn.read(conn.inWaiting())
+
+
+def save_data(temperature, salinity, oxygen):
+	# TODO save data to database (sqlite)
+	pass
+
+
+def push_data(temperature, salinity, oxygen, server_ip, server_port):
+	# TODO push data to lighthouse node.
+	pass
+
+
+def initialize_serial_connections(oxy_usb, sal_usb):
+	temp_conn = create_connection(temp_usb)
+	sal_conn = create_connection(sal_usb)
+	oxy_conn = create_connection(oxy_usb)
+
+	disable_continuous_mode(temp_conn)
+	disable_continuous_mode(sal_conn)
+	disable_continuous_mode(oxy_conn)
+
+	return temp_conn, sal_conn, oxy_conn
+
+
+def run_loop(oxy_usb, sal_usb, server_ip, server_port):
+	temp_conn, sal_conn, oxy_conn = initialize_serial_connections()
+
 	# TODO: Catch serial.serialutil.SerialException on read?
 	while True:
-		data = temperature_ser.read().decode('ascii')
+		temp.write('R\r')
+		temp = temp_conn.readline()
+
+		sal.write('R\r')
+		sal = sal_conn.readline()
 		
-		if(data == "\r"):
-			process_line(line)
-			line = ""
-		else:
-			line = line + data
+		# TODO: send temp and sal to oxy sensor first, then retrieve oxy value.
+		# oxy.write(<salinity command here>)
+		# oxy.write(<temp command here>)
+		oxy.write('R\r')
+		oxy = oxy_conn.readline()
 
-temperature_thread = threading.Thread(target=temperature_loop)
-temperature_thread.start()
+		print('Temperature: {}, Dissolved Oxygen: {}, Salinity: {}'.format(temp, oxy, sal))
+		
+		save_data(temp, oxy, sal)
+		push_data(temp, oxy, sal, server_ip, server_port)
+
+		# TODO: Determine how often we should be grabbing data from sensors and pushing to other pi node.
+		time.sleep(5)
 
 
-"""
- TODO:
-	Add command line args for USB ports for Salinity and Dissolved Oxygen sensors.
-	
-	On startup disable continuous send mode on each sensor.
-	
-	Create run loop that reads each value individually then pushes data to other pi node.
-
-	Inside run loop send temperature and salinity values to dissolved oxygen sensor before reading.
-	
-	Determine how often we should be grabbing data from sensors and pushing to other pi node.
-
-	Create sqllite database to store data in.
-
-	Create method for extracting CSV of data.
-
-	Create supervisord script to keep run.py running.
-"""
+if __name__ == '__main__':
+	# TODO: Create supervisord script to keep run.py running.
+	# TODO: Parse command line args for Salinity and Dissolved oxygen serial ports, append them to args array.
+	# TODO: Parse command line args for lighthouse ip address and port, append them to args array.
+	# TODO: Parse command line args for database connection info.
+	init_db()
+	args = []
+	temperature_thread = threading.Thread(target=run_loop, args=args)
+	temperature_thread.start()
