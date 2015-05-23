63ed7529495435d47faaadf9547ac79644ede08a
alan <alan@fancysandwiches.com>
2015-05-23 14:24:07 -0700

    [run.py] Convert tabs to spaces, fix imports.

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import argparse
-import serial
-import threading
-from io import BufferedRWPair, TextIOWrapper
-from time import sleep
+import time
 
+import serial
+from io import BufferedRWPair, TextIOWrapper
 
 temp_usb = '/dev/ttyAMA0'
 BAUD_RATE = 9600
@@ -15,78 +14,80 @@ parser.add_argument('port', help='The port of the lighthouse node.')
 
 
 def init_db():
-	# TODO: initialize the sqlite database.
-	pass
+    # TODO: initialize the sqlite database.
+    pass
 
 
 def create_connection(usb_port):
-	print('Creating connection on {}'.format(usb_port))
-	ser = serial.Serial(usb_port, BAUD_RATE)
-	# disable_continuous_mode(ser)
-	return TextIOWrapper(BufferedRWPair(ser, ser), newline='\r', encoding='ascii', line_buffering=True)
+    print('Creating connection on {}'.format(usb_port))
+    ser = serial.Serial(usb_port, BAUD_RATE)
+    # disable_continuous_mode(ser)
+    return TextIOWrapper(BufferedRWPair(ser, ser), newline='\r', encoding='ascii', line_buffering=True)
 
 
 def disable_continuous_mode(conn: serial.Serial):
-	# TODO: research if we need to send this command every time we connect to the sensors, or if it only
-	# needs to be sent once to disable continuous mode. If only once we should move this code into a
-	# separate python file.
-	print('Disabling continuous mode...')
-	conn.write(bytes('E\r', 'ascii'))
+    # TODO: research if we need to send this command every time we connect to the sensors, or if it only
+    # needs to be sent once to disable continuous mode. If only once we should move this code into a
+    # separate python file.
+    print('Disabling continuous mode...')
+    conn.write(bytes('E\r', 'ascii'))
 
-	if conn.inWaiting() > 0:
-		# clear the buffer if there is anything waiting.
-		print('Clearing buffer...')
-		conn.read(conn.inWaiting())
+    if conn.inWaiting() > 0:
+        # clear the buffer if there is anything waiting.
+        print('Clearing buffer...')
+        conn.read(conn.inWaiting())
 
 
 def save_data(temperature, salinity, oxygen):
-	# TODO save data to database (sqlite)
-	pass
+    # TODO save data to database (sqlite)
+    pass
 
 
 def push_data(temperature, salinity, oxygen, server_ip, server_port):
-	payload = {'temperature': temperature, 'salinity': salinity, 'oxygen': oxygen}
-	# TODO push data to lighthouse node.
+    payload = {'temperature': temperature, 'salinity': salinity, 'oxygen': oxygen}
+
+
+# TODO push data to lighthouse node.
 
 
 def initialize_serial_connections(oxy_usb, sal_usb):
-	temp_conn = create_connection(temp_usb)
-	sal_conn = create_connection(sal_usb)
-	oxy_conn = create_connection(oxy_usb)
+    temp_conn = create_connection(temp_usb)
+    sal_conn = create_connection(sal_usb)
+    oxy_conn = create_connection(oxy_usb)
 
-	return temp_conn, sal_conn, oxy_conn
+    return temp_conn, sal_conn, oxy_conn
 
 
 def run_loop(oxy_usb, sal_usb, server_ip, server_port):
-	init_db()
-	temp_conn, sal_conn, oxy_conn = initialize_serial_connections(oxy_usb, sal_usb)
+    init_db()
+    temp_conn, sal_conn, oxy_conn = initialize_serial_connections(oxy_usb, sal_usb)
 
-	# TODO: Catch serial.serialutil.SerialException on read?
-	while True:
-		temp_conn.write('R\r')
-		temp = temp_conn.readline()
+    # TODO: Catch serial.serialutil.SerialException on read?
+    while True:
+        temp_conn.write('R\r')
+        temp = temp_conn.readline()
 
-		sal_conn.write('R\r')
-		sal = sal_conn.readline()
-		
-		# TODO: send temp and sal to oxy sensor first, then retrieve oxy value.
-		# oxy.write(<salinity command here>)
-		# oxy.write(<temp command here>)
+        sal_conn.write('R\r')
+        sal = sal_conn.readline()
 
-		oxy_conn.write('R\r')
-		oxy = oxy_conn.readline()
+        # TODO: send temp and sal to oxy sensor first, then retrieve oxy value.
+        # oxy.write(<salinity command here>)
+        # oxy.write(<temp command here>)
 
-		print('Temperature: {}, Dissolved Oxygen: {}, Salinity: {}'.format(temp, oxy, sal))
+        oxy_conn.write('R\r')
+        oxy = oxy_conn.readline()
 
-		save_data(temp, oxy, sal)
-		push_data(temp, oxy, sal, server_ip, server_port)
+        print('Temperature: {}, Dissolved Oxygen: {}, Salinity: {}'.format(temp, oxy, sal))
 
-		# TODO: Determine how often we should be grabbing data from sensors and pushing to other pi node.
-		time.sleep(5)
+        save_data(temp, oxy, sal)
+        push_data(temp, oxy, sal, server_ip, server_port)
+
+        # TODO: Determine how often we should be grabbing data from sensors and pushing to other pi node.
+        time.sleep(5)
 
 
 if __name__ == '__main__':
-	# TODO: Create supervisord script to keep run.py running.
-	# TODO: Parse command line args for database connection info.
-	args = parser.parse_args()
-	run_loop(args.oxygen, args.salinity, args.server_ip, args.port)
+    # TODO: Create supervisord script to keep run.py running.
+    # TODO: Parse command line args for database connection info.
+    args = parser.parse_args()
+    run_loop(args.oxygen, args.salinity, args.server_ip, args.port)
