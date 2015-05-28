dd416cf649e8bbda1219bc188597312004ed2b48
crdietrich <hg@wildjuniper.com>
2015-05-27 21:52:34 -0700

    Addition of i2c sensor calls to main run loop.

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ import serial
 from io import BufferedRWPair, TextIOWrapper
 
 from cso_parser import CsoParser
+from sensors import AtlasI2C
 
-temp_usb = '/dev/ttyAMA0'
-BAUD_RATE = 9600
+# sensor class, preconfigured for Pi B+ v2
+ai2c = AtlasI2C()
+
 parser = argparse.ArgumentParser()
-parser.add_argument('oxygen', help='The USB port of the oxygen sensor.')
-parser.add_argument('salinity', help='The USB port of the salinity sensor.')
 parser.add_argument('server_ip', help='The IP address of the lighthouse node.')
 parser.add_argument('port', help='The port of the lighthouse node.')
 
@@ -18,27 +18,6 @@ parser.add_argument('port', help='The port of the lighthouse node.')
 def init_db():
     # TODO: initialize the sqlite database.
     pass
-
-
-def create_connection(usb_port):
-    print('Creating connection on {}'.format(usb_port))
-    ser = serial.Serial(usb_port, BAUD_RATE)
-    # disable_continuous_mode(ser)
-    return TextIOWrapper(BufferedRWPair(ser, ser), newline='\r', encoding='ascii', line_buffering=True)
-
-
-def disable_continuous_mode(conn: serial.Serial):
-    # TODO: research if we need to send this command every time we connect to the sensors, or if it only
-    # needs to be sent once to disable continuous mode. If only once we should move this code into a
-    # separate python file.
-    print('Disabling continuous mode...')
-    conn.write(bytes('E\r', 'ascii'))
-
-    if conn.inWaiting() > 0:
-        # clear the buffer if there is anything waiting.
-        print('Clearing buffer...')
-        conn.read(conn.inWaiting())
-
 
 def save_data(temperature, salinity, oxygen, cso_now, cso_recent):
     # TODO save data to database (sqlite)
@@ -52,46 +31,32 @@ def push_data(temperature, salinity, oxygen, cso_now, cso_recent, server_ip, ser
 
 # TODO push data to lighthouse node.
 
+# def run_loop(oxy_usb, sal_usb, server_ip, server_port):
+def run_loop(server_ip, server_port):
 
-def initialize_serial_connections(oxy_usb, sal_usb):
-    temp_conn = create_connection(temp_usb)
-    sal_conn = create_connection(sal_usb)
-    oxy_conn = create_connection(oxy_usb)
-
-    return temp_conn, sal_conn, oxy_conn
-
-
-def run_loop(oxy_usb, sal_usb, server_ip, server_port):
     init_db()
     cso_parser = CsoParser()
-    temp_conn, sal_conn, oxy_conn = initialize_serial_connections(oxy_usb, sal_usb)
+    #temp_conn, sal_conn, oxy_conn = initialize_serial_connections(oxy_usb, sal_usb)
 
-    # TODO: Catch serial.serialutil.SerialException on read?
     while True:
-        temp_conn.write('R\r')
-        temp = temp_conn.readline()
 
-        sal_conn.write('R\r')
-        sal = sal_conn.readline()
+        s = ai2c.query_all()
+        temp = s[1]
+        do = s[2]
+        ph = s[3]
 
-        # TODO: send temp and sal to oxy sensor first, then retrieve oxy value.
-        # oxy.write(<salinity command here>)
-        # oxy.write(<temp command here>)
+        print("Time,Temp,DO,pH,EC,TDS,SAL,SG,Status")
+        print(s)
 
-        oxy_conn.write('R\r')
-        oxy = oxy_conn.readline()
-
-        print('Temperature: {}, Dissolved Oxygen: {}, Salinity: {}'.format(temp, oxy, sal))
-
-        save_data(temp, oxy, sal, cso_parser.now_count, cso_parser.recent_count)
-        push_data(temp, oxy, sal, server_ip, cso_parser.now_count, cso_parser.recent_count, server_port)
+        save_data(temp, do, ph, cso_parser.now_count, cso_parser.recent_count)
+        push_data(temp, do, ph, cso_parser.now_count, cso_parser.recent_count)
 
         # TODO: Determine how often we should be grabbing data from sensors and pushing to other pi node.
         time.sleep(5)
-
 
 if __name__ == '__main__':
     # TODO: Create supervisord script to keep run.py running.
     # TODO: Parse command line args for database connection info.
     args = parser.parse_args()
-    run_loop(args.oxygen, args.salinity, args.server_ip, args.port)
+    # run_loop(args.oxygen, args.salinity, args.server_ip, args.port)
+    run_loop(args.server_ip, args.port)
