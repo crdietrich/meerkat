72c8fc08b4b19b065e714f48b136b9a480a48927
crdietrich <repos@wildjuniper.com>
2017-12-09 18:45:36 -0800

    update base with datapackage methods

--- base.py
+++ base.py
@@ -10,6 +10,12 @@ except:
     import struct
 
 
+iso_time_fmt = '%Y-%m-%dT%H:%M:%S%z'
+
+def generate_UUID():
+    # placeholder for UUID    
+    return 'non-compliant-UUID'
+
 def scan_I2C(i2c_bus):
     found_address = i2c_bus.scan()
     print('Found I2C devices at:', found_address)
@@ -82,37 +88,170 @@ def twos_comp_to_dec(value, bits):
     return value
 
 
-class Data(dict):
+class Resource(object):
+    """Base metadata for a data resource using the
+    Frictionless Data Specification
+    frictionlessdata.io
+    """
 
     def __init__(self, name):
-        """This whole thing needs to be converted to DataPackage format"""
-
-        self['name'] = name
-        self['sample_name'] = None
-        self['sample_id'] = None
-        self['datetime'] = None
-        self['lat'] = None
-        self['lon'] = None
-
-        self['names'] = None
-        self['values'] = None
-
-
-    def payload(self):
-        return self
+        self.name = name
+        self.profile = None  # TBD?
+        self.title = None
+        self.description = None
+        self.format = None
+        self.mediatype = 'text/csv'
+        self.encoding = 'utf-8'
+        self.bytes = None
+        self.hash = None
+        self.schema = None
+        self.sources = None
+        self.licenses = None
 
     def dumps(self):
-        return json.dumps(self.payload())
+        return json.dumps(vars(self))
 
-    def loads(self, data):
-        print(json.loads(data))
 
-        
-class Device(Data):
+class ResourcePath(Resource):
+    """Tabular data resource referenced in JSON metadata,
+    using the Frictionless Data Specification
+    frictionlessdata.io
+    """
+
+    def __init__(self, name, path):
+        super(ResourcePath, self).__init__(name)
+        self.path = path
+
+class ResourceData(Resource):
+    """Tabular data inline with JSON data resource,
+    using the Frictionless Data Specification
+    frictionlessdata.io
+    """
 
     def __init__(self, name):
-        super().__init__(name)
+        super(ResourceData, self).__init__(name)
+        self.data = None
 
+
+class DataDialect(object):
+    def __init__(self):
+
+        # attributes based on Frictionless Data CSV dialect
+        self.delimiter = ','
+        self.line_terminator = '\n'
+        self.quote_char = '"'
+        self.double_quote = True
+        self.escape_char = '\\'
+        self.null_sequence = 'NA'
+
+    def __repr__(self):
+        return json.dumps(vars(self))
+
+        #return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+
+
+class CSVDialect(DataDialect):
+    def __init__(self):
+        #super(DataDialect, self).__init__()
+        
+        # attributes based on Frictionless Data CSV dialect
+        self.delimiter = ','
+        self.line_terminator = '\n'
+        self.quote_char = '"'
+        self.double_quote = True
+        self.escape_char = '\\'
+        self.null_sequence = 'NA'
+
+        self.skip_initial_space = True
+        self.header = True
+        self.case_sensitive_header = False
+        self.csvddf_version = 'meerkat_0.1 based on frictionless.io v1.2'
+
+        # additional attributes
+        self.comment_char = '#'
+
+    def __repr__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+
+
+
+class JSONDialect(DataDialect):
+    def __init__(self):
+        super(DataDialect, self).__init__()
+
+    def __repr__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+
+
+class DataFormat(object):
+
+    def __init__(self, format=None):
+        """This whole thing needs to be converted to DataPackage format"""
+        
+        self.format = format.lower()
+
+        # set dialect based on format
+        if self.format == 'csv':
+            self.dialect = CSVDialect()
+        elif self.format == 'json':
+            self.dialect = JSONDialect()
+        else:
+            self.dialect = CSVDialect()
+
+        # data collection attributes
+        self.sample_name = None
+        self.sample_id = None
+        self.datetime = None
+        self.lat = None
+        self.lon = None
+
+        self.names = None
+        self.values = None
+
+    def __repr__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+
+
+class CSVRecorder(object):
+    def __init__(self, device, description, header, folder):
+        self.device = device        
+        self.data_format = DataFormat('csv')
+        self.data_format.device = str(self.device)
+        
+        self.description = description
+        self.header = header
+        self.folder = folder  # local location to save .csv
+
+    #def __enter__(self):
+        
+        self.resource = ResourcePath(name=self.description,
+                                     path=self.description+'.csv')
+
+        self.f = open(folder+self.description+'.csv', 'w')
+        self.f.write(self.data_format.dialect.comment_char +
+                     str(self.data_format))
+        self.f.write(self.data_format.dialect.line_terminator)
+        self.f.write(self.header)
+        self.f.write(self.data_format.dialect.line_terminator)
+
+    def __exit__(self):
+        self.f.close()
+
+
+    def write(self, sample_data):
+        """
+        sample_data : one line of data collected in a multi sample sweep/session
+        """
+
+        self.f.write(sample_data)
+        self.f.write(self.data_format.dialect.line_terminator)
+        
+        
+class Device(object):
+
+    def __init__(self, device_name):
+        
+        self.device_name = device_name
         self.description = None
         self.urls = None
 
@@ -125,5 +264,12 @@ class Device(Data):
         self.version_sw = None
         self.accuracy = None
         self.precision = None
+        self.dtype = None
         self.calibration_date = None
+
+    def __repr__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
+
+    def dumps(self):
+        return json.dumps(vars(self))
 
