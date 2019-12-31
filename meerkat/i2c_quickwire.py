bd79c93bc02f2dcee04887b9013cb8b7dd24f67a
Colin Dietrich <repos@wildjuniper.com>
2019-12-31 11:55:43 -0800

    update quickwire license info & add pymakr config

--- meerkat/i2c_quickwire.py
+++ meerkat/i2c_quickwire.py
@@ -1,26 +1,44 @@
-# Copied from https://github.com/PiSupply/Flick
-# 2019-03-13 CRD
+"""A Python library for controlling I2C devices
+connected to the Raspberry Pi
 
-# Adapted from https://github.com/quick2wire/quick2wire-python-api
-# And packaged into an i2c only module for convinience
+Copyright (c) 2012 Quick2Wire Limited
+Copyright (c) 2014 Pimoroni Ltd
 
-# Warning: not part of the published Quick2Wire API.
-#
-# Converted from i2c.h and i2c-dev.h
-# I2C only, no SMB definitions
+Developed originally by Quick2Wire and released with an MIT and LPL license
+Modified by Pimoroni Ltd and released with a MIT license
+
+MIT License
+-----------
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Copied from https://github.com/PiSupply/Flick
+2019-03-13 CRD
+
+Pimoroni adapted from https://github.com/quick2wire/quick2wire-python-api
+And packaged into an i2c only module for convinience
+
+Warning: not part of the published Quick2Wire API.
+
+Converted from i2c.h and i2c-dev.h
+I2C only, no SMB definitions
+"""
 
 from ctypes import c_int, c_uint16, c_ushort, c_short, c_ubyte, c_char, POINTER, Structure
 
 # /usr/include/linux/i2c-dev.h: 38
 class i2c_msg(Structure):
     """<linux/i2c-dev.h> struct i2c_msg"""
-    
+
     _fields_ = [
         ('addr', c_uint16),
         ('flags', c_ushort),
         ('len', c_short),
         ('buf', POINTER(c_char))]
-    
+
     __slots__ = [name for name,type in _fields_]
 
 
@@ -51,15 +69,15 @@ I2C_FUNC_PROTOCOL_MANGLING	= 0x00000004 # I2C_M_NOSTART etc.
 
 # ioctls
 
-I2C_SLAVE	= 0x0703	# Change slave address			
-				# Attn.: Slave address is 7 or 10 bits  
-I2C_SLAVE_FORCE	= 0x0706	# Change slave address			
-				# Attn.: Slave address is 7 or 10 bits  
-				# This changes the address, even if it  
-				# is already taken!			
-I2C_TENBIT	= 0x0704	# 0 for 7 bit addrs, != 0 for 10 bit	
-I2C_FUNCS	= 0x0705	# Get the adapter functionality         
-I2C_RDWR	= 0x0707	# Combined R/W transfer (one stop only) 
+I2C_SLAVE	= 0x0703	# Change slave address
+				# Attn.: Slave address is 7 or 10 bits
+I2C_SLAVE_FORCE	= 0x0706	# Change slave address
+				# Attn.: Slave address is 7 or 10 bits
+				# This changes the address, even if it
+				# is already taken!
+I2C_TENBIT	= 0x0704	# 0 for 7 bit addrs, != 0 for 10 bit
+I2C_FUNCS	= 0x0705	# Get the adapter functionality
+I2C_RDWR	= 0x0707	# Combined R/W transfer (one stop only)
 
 
 import sys
@@ -87,51 +105,51 @@ default_bus = 1 if revision() > 1 else 0
 
 class I2CMaster(object):
     """Performs I2C I/O transactions on an I2C bus.
-    
+
     Transactions are performed by passing one or more I2C I/O messages
     to the transaction method of the I2CMaster.  I2C I/O messages are
     created with the reading, reading_into, writing and writing_bytes
     functions defined in the quick2wire.i2c module.
-    
+
     An I2CMaster acts as a context manager, allowing it to be used in a
     with statement.  The I2CMaster's file descriptor is closed at
     the end of the with statement and the instance cannot be used for
     further I/O.
-    
+
     For example:
-    
+
         from quick2wire.i2c import I2CMaster, writing
-        
+
         with I2CMaster() as i2c:
             i2c.transaction(
                 writing(0x20, bytes([0x01, 0xFF])))
     """
-    
+
     def __init__(self, n=default_bus, extra_open_flags=0):
         """Opens the bus device.
-        
+
         Arguments:
         n                -- the number of the bus (default is
                             the bus on the Raspberry Pi accessible
                             via the header pins).
-        extra_open_flags -- extra flags passed to posix.open when 
-                            opening the I2C bus device file (default 0; 
+        extra_open_flags -- extra flags passed to posix.open when
+                            opening the I2C bus device file (default 0;
                             e.g. no extra flags).
         """
         self.fd = posix.open("/dev/i2c-%i"%n, posix.O_RDWR|extra_open_flags)
-    
+
     def __enter__(self):
         return self
-    
+
     def __exit__(self, exc_type, exc_value, traceback):
         self.close()
-    
+
     def close(self):
         """
         Closes the I2C bus device.
         """
         posix.close(self.fd)
-    
+
     def transaction(self, *msgs):
         """
         Perform an I2C I/O transaction.
@@ -139,19 +157,19 @@ class I2CMaster(object):
         Arguments:
         *msgs -- I2C messages created by one of the reading, reading_into,
                  writing or writing_bytes functions.
-        
-        Returns: a list of byte sequences, one for each read operation 
+
+        Returns: a list of byte sequences, one for each read operation
                  performed.
         """
-        
+
         msg_count = len(msgs)
         msg_array = (i2c_msg*msg_count)(*msgs)
         ioctl_arg = i2c_rdwr_ioctl_data(msgs=msg_array, nmsgs=msg_count)
-        
+
         ioctl(self.fd, I2C_RDWR, ioctl_arg)
-        
+
         return [i2c_msg_to_bytes(m) for m in msgs if (m.flags & I2C_M_RD)]
-	
+
     def get(self, addr, n_bytes):
         return self.transaction(reading(addr,n_bytes))
 
@@ -175,15 +193,15 @@ def reading_into(addr, buf):
     return _new_i2c_msg(addr, I2C_M_RD, buf)
 
 def writing_bytes(addr, *bytes):
-    """An I2C I/O message that writes one or more bytes of data. 
-    
+    """An I2C I/O message that writes one or more bytes of data.
+
     Each byte is passed as an argument to this function.
     """
     return writing(addr, bytes)
 
 def writing(addr, byte_seq):
     """An I2C I/O message that writes one or more bytes of data.
-    
+
     The bytes are passed to this function as a sequence.
     """
     buf = bytes(byte_seq)
