0af3cc2f3a34539515cbffaf8d24dff65c32ae4b
Colin Dietrich <repos@wildjuniper.com>
2020-01-01 15:21:54 -0800

    Change test_relay i2c bus detection to use sys module

--- tests/test_relay.py
+++ tests/test_relay.py
@@ -1,8 +1,16 @@
 # main.py -- put your code here!
+import sys
+
 from meerkat import relay
 from meerkat.base import time
 
-r = relay.Single(0, 0x18, "json")
+
+if sys.platform == "linux":
+    i2c_bus = 1
+else:
+    i2c_bus = 0
+
+r = relay.Single(i2c_bus, 0x18, "json")
 
 print("Status: {}".format(r.get_status()))
 print("---")
@@ -18,6 +26,7 @@ print("Relay set to on")
 print("Status: {}".format(r.get_status()))
 print("---")
 
+# this should generate a header file of json metadata, then lines of data
 for n in range(5):
     r.toggle(verbose=True)
     r.write(description="toggle {}".format(n))
