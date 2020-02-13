327d4fe1cfeb32bf4a5276967ade19b86ada7541
Colin Dietrich <repos@wildjuniper.com>
2020-02-12 21:43:08 -0800

    just dev config updates

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # main.py -- put your code here!
 
-from meerkat import ina219_test
+# from meerkat import 
 from meerkat.tools import dev_clean, ls
+
+# allow function call without brackets
+dev_clean = dev_clean()
+ls = ls()
