924f1c5b8a438f42ded5864d56e46d6d8bae0b66
Colin Dietrich <repos@wildjuniper.com>
2020-02-22 15:36:52 -0800

    Add generic main.py with comments

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # main.py -- put your code here!
 
+# tools for development work
+# ls = list directly alias for os.listdir()
+# dev_clean = remove .csv and .jsontxt files from flash storage
 from meerkat.tools import dev_clean, ls
 
-#from meerkat import atlas_cond_test
+# import a test file, i.e. ina219_test.py
+#from meerkat import
