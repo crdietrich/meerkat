8e2bdc1b536f1aceb55d760fd7f7c7af0ce0bab3
Colin Dietrich <repos@wildjuniper.com>
2020-12-26 21:21:31 -0800

    Update Grove motor_dc_test.py and motor_step_test.py

--- tests/motor_dc_test.py
+++ tests/motor_dc_test.py
@@ -1,37 +1,36 @@
 """Test the Grove Motor Controller stepper function"""
 
-import sys
 
 from meerkat import motor
-from meerkat.base import time
+from meerkat.base import time, i2c_default_bus
 
-if sys.platform == "linux":
-    i2c_bus = 1
-else:
-    i2c_bus = 0
+m = motor.GroveMotor(bus_n=i2c_default_bus)
 
-m = motor.GroveMotor(bus_n=i2c_bus)
+m = motor.GroveMotor(bus_n=i2c_default_bus)
 m.stop()
+
+print()
 print("> Initially set to stopped")
 print()
 
 m.set_mode(mode_type="dc")
 m.set_frequency(f_Hz=31372)
 
+print()
 print("> Device Description")
-print(m.device)
+print(m.metadata)
 print()
 print("> Data Output Header")
-print(m.csv_writer.header)
+print(m.metadata.header)
 print()
 print("> Initial State")
 print(m.get("test_1_init"))
 print()
 print("> DC Motor Demo")
 print("  =============")
-
+print()
 print("> Full speed Clockwise and Counter Clockwise")
-
+print()
 for d in ["cw", "ccw"]:
     m.set_direction(d)
     m.set_speed(motor_id=1, motor_speed=255)
@@ -44,9 +43,10 @@ for d in ["cw", "ccw"]:
     print(m.get("test_5d_dc_{}".format(d)))
     m.write("test_5e_dc_{}".format(d))
     print(m.publish("test_5f_dc_{}".format(d)))
-
+    print()
 m.stop()
 time.sleep(0.5)
+
 print("> Ramp up motor speed Clockwise")
 for speed in range(0, 255, 25):
     m.set_speed(motor_id=1, motor_speed=speed)
@@ -55,8 +55,8 @@ for speed in range(0, 255, 25):
     m.write(description=desc)
     print("JSON Format:", m.publish(description=desc))
     time.sleep(0.25)
+    print()
 m.stop()
 
 print()
 print("All tests done.")
-print("Data saved to file: {}".format(m.csv_writer.path))
